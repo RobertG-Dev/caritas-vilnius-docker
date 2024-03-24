8fc4731e8f098e01dc815bad0137dbf13bab9576
RobertG-Dev <robertas.grajevskis@gmail.com>
2024-03-24 12:09:33 +0200

    Fix path; remove unnecessary files

--- bin/databases/create-test-databases.sql
+++
@@ -1,6 +0,0 @@
-CREATE USER
-    IF NOT EXISTS 'wp_test_user' @'%' IDENTIFIED BY 'password';
-
-CREATE DATABASE IF NOT EXISTS `wp_test_db`;
-
-GRANT ALL PRIVILEGES ON `wp_test_db`.* TO 'wp_test_user'@'%';
